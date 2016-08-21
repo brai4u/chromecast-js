85f934b2554b75d3ef704b2635eedf384448bb7e
Brian Ardiles <ardiles.brian@gmail.com>
2016-08-21 06:26:26 +0200

    fix

--- index.js
+++ index.js
@@ -1,49 +1,50 @@
-'use strict';
-
-var events = require('events');
+var util = require( 'util' );
+var events = require( 'events' );
+var ssdp = require('node-ssdp').Client;
 var http = require('http');
-var util = require('util');
-
 var Device = require('./device').Device;
-var SsdpClient = require('node-ssdp').Client;
+var debug = require('debug')('chromecast-js');
 
-function Browser(options) {
-  events.EventEmitter.call(this);
-  this.init(options);
-}
+exports.Device = Device;
 
-Browser.prototype.update = function(device) {
-  var devConfig = {addresses: device.addresses, name: device.name};
-  this.device = new Device(devConfig);
-  this.emit('deviceOn', this.device);
+var Browser = function( options ) {
+  events.EventEmitter.call( this );
+  this.init( options );
 };
 
-Browser.prototype.init = function() {
-  var _this = this;
+util.inherits(Browser, events.EventEmitter );
 
-  var ssdpBrowser = new SsdpClient();
-  ssdpBrowser.on('response', function(headers, statusCode, rinfo) {
-    if (statusCode !== 200 || !headers.LOCATION) {
-      return;
-    }
-    http.get(headers.LOCATION, function(res) {
-      var body = '';
-      res.on('data', function(chunk) {
-        body += chunk;
-      });
-      res.on('end', function() {
-        var match = body.match(/<friendlyName>(.+?)<\/friendlyName>/);
-        if (!match || match.length !== 2) {
-          return;
-        }
-        _this.update({addresses: [rinfo.address], name: match[1]});
-      });
-    });
+exports.Browser = Browser;
+
+Browser.prototype.update = function( device ) {
+    var dev_config = {addresses: device.addresses, name: device.name};
+    this.device = new Device(dev_config);
+    this.emit('deviceOn', this.device);
+}
+
+Browser.prototype.init = function( options ) {
+  var self = this;
+
+  var ssdpBrowser = new ssdp();
+  ssdpBrowser.on('response', function (headers, statusCode, rinfo) {
+      if (statusCode != 200)
+		  return;
+	  if (!headers['LOCATION'])
+		  return;
+	  var request = http.get(headers['LOCATION'], function(res) {
+		  var body = '';
+		  res.on('data', function(chunk) {
+		    body += chunk;
+		  });
+		  res.on('end', function() {
+			  if (body.search('<manufacturer>Google Inc.</manufacturer>') == -1)
+				  return;
+			  var match = body.match(/<friendlyName>(.+?)<\/friendlyName>/);
+			  if (!match || match.length != 2)
+				  return;
+			  self.update({addresses: [rinfo.address], name: match[1]});
+		  });
+	  });
   });
   ssdpBrowser.search('urn:dial-multiscreen-org:service:dial:1');
 };
-
-util.inherits(Browser, events.EventEmitter);
-
-exports.Browser = Browser;
-exports.Device = Device;
